24e7b39b3245f6990585a091fe18a1457fadb8f2
Nicanor Kyamba <nicanorkyamba98@gmail.com>
2024-08-02 17:37:26 +0300

    bug: Removed unsused imports

--- src/apolloClient.ts
+++ src/apolloClient.ts
@@ -1,18 +1,4 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
-// import { createUploadLink } from 'apollo-upload-client';
-import createUploadLink from 'apollo-upload-client/createUploadLink.mjs';
-// import { setContext } from '@apollo/client/link/context';
-
-// // Create an authentication link to add the token to headers
-// const authLink = setContext((_, { headers }) => {
-//     const token = localStorage.getItem('token'); // Retrieve token from localStorage
-//     return {
-//         headers: {
-//             ...headers,
-//             authorization: token ? `Bearer ${token}` : '', // Add token to authorization header if it exists
-//         },
-//     };
-// });
 
 // Create the Apollo Client instance
 export const client = new ApolloClient({
